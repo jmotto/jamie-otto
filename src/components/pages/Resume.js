c07c6d915638306aa6906a9408d65e3141095079
Jamie Otto <jmotto31@gmail.com>
2023-02-16 12:26:14 -0800

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,13 @@
 import { Card, Link } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
-import MyResume from '../../images/resume.png'
+import MyResume from '../../images/otto-resume.png'
 
 
 export const Resume = () => {
     return(
         <Container >
-            <Link variant='h3' href="https://drive.google.com/file/d/1h5NegitGL_XVk9dEpVx7xj1UXAhFUU-k/view?usp=sharing" alt="resume">
+            <Link variant='h3' href="https://drive.google.com/file/d/16XfS3z1jVHZTSSatj5M4-Qu6hSKftDRu/view?usp=sharing" alt="resume" target="_blank" rel="noopener noreferrer">
             <Card>
             <img height="500px" src={MyResume} alt="resume"/>
             </Card>
