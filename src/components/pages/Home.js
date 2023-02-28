b4f70fb4ec90768431592e1feeb5687f3bd4456f
Jamie Otto <jmotto31@gmail.com>
2023-02-28 11:06:53 -0800

    added email to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -21,14 +21,19 @@ export const Home = () => {
                     Hello! 
                     </Typography>
                     <Typography variant="body2" margin="20px" justifyContent="center">
-                    I am a full stack web developer with experience with web technologies such as HTML, CSS, databases, JavaScript, React, Node, and Express, GitHub. When I am not coding, I am spending time with my family and exploring nature.
+                     I am a full stack web developer with experience with web technologies such as HTML, CSS, databases, JavaScript, React, Node, and Express, GitHub. When I am not coding, I am spending time with my family and exploring nature.
+                     Contact me at jmotto.dev@gmail.com
                     </Typography>
+
                     <Stack  direction="row" style={{ justifyContent: "center", display: "flex" }} spacing={1}> 
                     <Avatar 
                     sx={{ width: 200, height: 200, }} src={MyPhoto} alt="Me outside" 
                     />
+                 
                     </Stack>
-        
+                    <Typography>
+                    jmotto.dev@gmail.com
+                    </Typography>
                     <Typography variant="h6">Technologies I Know</Typography>
                         <div >
                         <DiJavascript1 />
